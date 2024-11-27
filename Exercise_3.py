717834b9ef05693e5b965cf273dbcf304b8b9e8e
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2024-11-27 16:51:14 +0100

    Merged exercise 3 into single file

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 25 14:24:02 2024
+Created on Mon Nov 25 17:31:27 2024
 
 @author: mendez
 """
@@ -11,196 +11,188 @@ from scipy.sparse.linalg import eigsh
 from scipy.spatial.distance import cdist
 from scipy.stats import qmc # sample method
 from scipy import linalg
+import os
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
+# Computation of all the w_u, w_b.
+import multiprocessing
+
+# # import functions from the previous script
+# from Exercise_3 import RBF_Collocation, Gamma_RBF, RBF_U_Reg, Evaluate_RBF
 
 import sys
 sys.path.insert(0, 'spicy_newrelease')
 from spicy_vki.spicy import Spicy
 
-# Setting for the plots
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rc('xtick',labelsize=13)
-plt.rc('ytick',labelsize=13)
+# Generate the output folder
+Fol_Plots = 'plots_exercise_3'
+if not os.path.exists(Fol_Plots):
+    os.makedirs(Fol_Plots)
 
-Name = 'PTV_DATA_CYLINDER/Res00017.dat'
+#%%
+
+
+Fol_In = 'PTV_DATA_CYLINDER'
+
+Fol_Out_A = Fol_Plots + os.sep + 'PTV_vs_RBF_Animation_CYL'
+if not os.path.exists(Fol_Out_A):
+    os.mkdir(Fol_Out_A)
+
+# This is a folder with all the PTV data
+Fol_Rbf = 'RBF_DATA_CYLINDER'
+if not os.path.exists(Fol_Rbf):
+    os.mkdir(Fol_Rbf)
+
+total_datasets = 1000
+file_names = sorted([file for file in os.listdir(Fol_In) if 'MESH' not in file])[:total_datasets]
+
+# We set up the collocation info. It is the same in each snapshot
+Name = Fol_In + os.sep + file_names[0]
 X_p, Y_p, U_p, V_p = np.genfromtxt(Name).T
 
-#%% Functions 1: RBF Collocation
-
-def RBF_Collocation(X_p, Y_p, R_max, R_b, gamma_max):
-    # Step 1: Define number of bases and collocate them
-    n_p = len(X_p); n_b = round(n_p/R_b)
-    # Define sampler and collocation points in [0,1]x [0,1]
-    sampler = qmc.Halton(d=2, scramble=True, seed=42)
-    collocation_points_01 = sampler.random(n=n_b)
-    domain_bounds = np.array([[np.min(X_p),np.max(X_p)],
-                            [np.min(Y_p),np.max(Y_p)]])
-    # Define the collocation points
-    collocation_points = qmc.scale(collocation_points_01,
-                                 domain_bounds[:,0],domain_bounds[:,1])
-    X_c, Y_c = collocation_points[:,0],collocation_points[:,1]
-    # plt.figure()
-    # plt.scatter(X_c, Y_c)
-    # Step 2 Define the shape factors
-    pairwise_distances = cdist(collocation_points,
-                               collocation_points)
-    c  = np.zeros(n_b)
-
-    for i in range(n_b):
-        distances = pairwise_distances[i]
-        distances[i] = np.inf  # Ignore self-distance
-        nearest_distance = np.min(distances)
-        # print(nearest_distance)
-        if nearest_distance > 0:
-            c[i] = np.sqrt(-np.log(gamma_max)) / nearest_distance
-        else:
-            c[i] = np.sqrt(-np.log(gamma_max)) / R_max # cap limit
-
-    return X_c, Y_c, c
-
-
-#%% Functions 2: Basis Matrix generation
-
-def Gamma_RBF(X_p, Y_p, X_C, Y_C, c_k):
-    n_b = len(X_C) # number of bases
-    n_p = len(X_p) # number of points for the evaluation
-    Gamma_matrix = np.zeros((n_p,n_b))
-    for r in range(n_b):
-        gaussian = np.exp(-c_k[r]**2 * ((X_p-X_C[r])**2 + (Y_p-Y_C[r])**2))
-        Gamma_matrix[:, r] = gaussian
-    return Gamma_matrix # matrix of basis functions
-
-#%% Functions 3: Regularize and solve system
-
-def RBF_U_Reg(X_p, Y_p, U_p, V_p, X_c, Y_c, c, K_cond=1e11):
-    Gamma_matrix = Gamma_RBF(X_p,Y_p,X_c,Y_c,c)
-    A = Gamma_matrix.T@Gamma_matrix
-    # return A
-    # Regularize it
-    lambda_A = eigsh(A, 1, return_eigenvectors=False) # Largest eigenvalue
-    alpha = (lambda_A-K_cond*2.2e-16) / K_cond
-    A = A+alpha*np.eye(np.shape(A)[0])
-    # Compute the RHSides
-    b_U = Gamma_matrix.T.dot(U_p);  b_V=Gamma_matrix.T.dot(V_p)
-    # Cholesky Factorization
-    L_A, low = linalg.cho_factor(A, overwrite_a = True, check_finite = False, lower = True)
-    # solve for w_u
-    w_u = linalg.cho_solve((L_A, low), b_U, check_finite = False)
-    # solve for w_v
-    w_v = linalg.cho_solve((L_A, low), b_V, check_finite = False)
-    return w_u, w_v
-
-#%% Function 4: evaluate a given RBF regression
-
-def Evaluate_RBF(X_g, Y_g, X_c, Y_c, c, w_u, w_v):
-    Gamma = Gamma_RBF(X_g, Y_g, X_c, Y_c, c) # basis on new points X_g, Y_g
-    U_reg = Gamma.dot(w_u)
-    V_reg = Gamma.dot(w_v)
-    return U_reg, V_reg
-
-
-R_b = 5 # number of particles per basis.
-gamma_max = 0.8
-R_max = 0.5
-X_c, Y_c, c = RBF_Collocation(X_p, Y_p, R_max, R_b, gamma_max)
-
-from time import time
-
-t1 = time()
-SP = Spicy(
-    data=[U_p, V_p],
-    points=[X_p, Y_p],
-    basis='gauss',
-    model='scalar',
-    verbose=1
-    )
-
+# We initialize the Spicy object
+SP = Spicy(points=[X_p, Y_p], data=[U_p, V_p], model='scalar', verbose=0)
+# And perform the random collocation. We also save these collocation points
 SP.collocation(n_K=[5], method='semirandom', r_mM=[0.001, 50], eps_l=0.8)
+collocation_path = Fol_Rbf + os.sep + 'RBFs.dat'  # name for the RBF info
+np.savetxt(collocation_path, np.column_stack((SP.X_C, SP.Y_C, SP.c_k)), delimiter='\t',fmt='%.6g')
 
-SP.scalar_constraints()
+# We extract them and plot the resulting circles. The collocation is relatively dense
+fig, ax = plt.subplots(figsize=(6, 3), dpi=100)
+ax.scatter(SP.X_C, SP.Y_C, c='r')
+for X_C, Y_C, d_k in zip(SP.X_C, SP.Y_C, SP.d_k):
+    circle = plt.Circle((X_C, Y_C), d_k,
+                        facecolor='None', edgecolor='k')
+    ax.add_patch(circle)
+ax.set_aspect('equal')
+ax.set_xlabel('$x[mm]$',fontsize=13)
+ax.set_ylabel('$y[mm]$',fontsize=13)
+ax.set_xticks(np.arange(0,70,10))
+ax.set_yticks(np.arange(-10,11,10))
+ax.set_xlim([0,50])
+ax.set_ylim(-10,10)
+circle = plt.Circle((0,0), 2.5, fill=True,
+                    color='r',edgecolor='k',alpha=0.5)
+ax.add_patch(circle)
+fig.tight_layout()
+fig.savefig(Fol_Plots + os.sep + 'RBF_collocation.png')
+
 
 SP.Assembly_Regression()
-SP.Solve(K_cond=1e12)
+SP.Solve(K_cond=1e11)
 
 n_x = 100; n_y = 50
 x_reg = np.linspace(np.min(X_p), np.max(X_p), n_x)
 y_reg = np.linspace(np.min(Y_p), np.max(Y_p), n_y)
 X_reg, Y_reg = np.meshgrid(x_reg, y_reg)
 
-U_reg, V_reg = SP.get_sol(points=[X_reg.ravel(), Y_reg.ravel()])
-
-print(np.linalg.norm(U_reg))
-print(np.linalg.norm(V_reg))
-
-w_U, w_V = RBF_U_Reg(X_p, Y_p, U_p, V_p, X_c, Y_c, c, K_cond=1e11)
-
-# Solve for the weights
-w_u, w_v = RBF_U_Reg(X_p, Y_p, U_p, V_p, X_c, Y_c, c, K_cond=1e11)
-# Get the solution on this new grid
-U_reg, V_reg = Evaluate_RBF(X_reg.reshape(-1),
-                            Y_reg.reshape(-1),
-                            X_c, Y_c, c, w_u, w_v)
-U_reg = U_reg.reshape(n_y, n_x)
-V_reg = V_reg.reshape(n_y, n_x)
-
-print(np.linalg.norm(U_reg))
-print(np.linalg.norm(V_reg))
-
-
-# fig, ax = plt.subplots(1, figsize=(6, 3))
-# # plt.quiver(X_p, Y_p, U_p, V_p)
-# plt.plot(X_c, Y_c, 'ro')
-# # plt.plot(X_C, Y_C, 'ko')
-# for i in range(len(c)):
-#     circle = plt.Circle([X_c[i], Y_c[i]], np.sqrt(np.log(2)/c[i]**2), edgecolor='blue', facecolor='none')
-#     ax.add_patch(circle)
-# ax.set_aspect('equal')
-# ax.set_xlabel('$x[mm]$',fontsize=13)
-# ax.set_ylabel('$y[mm]$',fontsize=13)
-# ax.set_xticks(np.arange(0,70,10))
-# ax.set_yticks(np.arange(-10,11,10))
-# ax.set_xlim([0,50])
-# ax.set_ylim(-10,10)
-# circle = plt.Circle((0,0),2.5,fill=True,color='r',edgecolor='k',alpha=0.5)
-# ax.add_patch(circle)
-# plt.tight_layout()
-# Name='RBF_Collocation.png'
-# plt.savefig(Name,dpi=100)
-# plt.close('all')
-
-
+U_reg, V_reg = SP.get_sol(points=[X_reg.ravel(), Y_reg.ravel()], shape=X_reg.shape)
 
 
 # reshape it back
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 3))
-axes[0].quiver(X_p,Y_p,U_p,V_p)
-axes[0].set_aspect('equal')
-axes[0].set_xlabel('$x[mm]$',fontsize=13)
-axes[0].set_ylabel('$y[mm]$',fontsize=13)
-axes[0].set_xticks(np.arange(0,70,10))
-axes[0].set_yticks(np.arange(-10,11,10))
-axes[0].set_xlim([0,50])
-axes[0].set_ylim(-10,10)
-circle = plt.Circle((0,0), 2.5, fill=True,
-                    color='r',edgecolor='k',alpha=0.5)
-axes[0].add_patch(circle)
-# axes[1].contourf(X_reg, Y_reg, U_reg.reshape(X_reg.shape), levels=30)
-axes[1].contourf(X_reg, Y_reg, V_reg.reshape(X_reg.shape), levels=30)
-# axes[1].contourf(X_reg, Y_reg, np.sqrt(U_reg**2+V_reg**2), levels=30)
-# axes[1].quiver(X_g.reshape(-1), Y_g.reshape(-1), U_reg, V_reg)
-axes[1].set_aspect('equal')
-axes[1].set_xlabel('$x[mm]$',fontsize=13)
-axes[1].set_ylabel('$y[mm]$',fontsize=13)
-axes[1].set_xticks(np.arange(0,70,10))
-axes[1].set_yticks(np.arange(-10,11,10))
-axes[1].set_xlim([0,50])
-axes[1].set_ylim(-10,10)
-circle = plt.Circle((0,0), 2.5, fill=True,
-                    color='r',edgecolor='k',alpha=0.5)
-axes[1].add_patch(circle)
-plt.tight_layout()
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 3), sharex=True, sharey=True)
+axes[0].quiver(X_p, Y_p, U_p, V_p)
+axes[1].contourf(X_reg, Y_reg, np.sqrt(U_reg**2+V_reg**2), levels=30)
+axes[1].quiver(X_reg, Y_reg, U_reg, V_reg)
+for ax in axes:
+    ax.set_aspect('equal')
+    ax.set_xlabel('$x[mm]$',fontsize=13)
+    ax.set_ylabel('$y[mm]$',fontsize=13)
+    ax.set_xticks(np.arange(0,70,10))
+    ax.set_yticks(np.arange(-10,11,10))
+    ax.set_xlim([0,50])
+    ax.set_ylim(-10,10)
+    circle = plt.Circle((0,0), 2.5, fill=True,
+                        color='r',edgecolor='k',alpha=0.5)
+    ax.add_patch(circle)
+fig.tight_layout()
+fig.savefig(Fol_Plots + os.sep + 'RBF_Regression_VS_PTV.png')
 
+#%% Parallel processing of the regressions
 
-# Name = 'RBF_Regression_VS_PTV.png'
-# plt.savefig(Name,dpi = 100)
-# plt.close('all')
+def process_parallel(file_name):
+    Name = Fol_In + os.sep + file_name
+    X_p, Y_p, U_p, V_p = np.genfromtxt(Name).T
+
+    SP = Spicy(points=[X_p, Y_p], data=[U_p, V_p], model='scalar', verbose=0)
+
+    # Instead of calling this collocation function
+    # SP.collocation(n_K=[5], method='semirandom', r_mM=[0.001, 50], eps_l=0.8)
+    # We assign it to be the same for all snapshots. This helps us a lot in exercise 5
+    SP.X_C, SP.Y_C, SP.c_k = np.genfromtxt(Fol_Rbf + os.sep + 'RBFs.dat').T
+
+    SP.Assembly_Regression()
+    SP.Solve(K_cond=1e11)
+
+    data = np.column_stack((SP.w_list[0], SP.w_list[1]))
+    np.savetxt(Fol_Rbf + os.sep + file_name, data, delimiter='\t',fmt='%.6g')
+
+num_workers = 4
+
+with ThreadPoolExecutor(max_workers=num_workers) as executor:
+    results = np.array(list(tqdm(executor.map(lambda file_name:
+                                              process_parallel(file_name),
+        file_names),
+        total=len(file_names), disable=False, desc='Processing with SPICY in parallel')))
+
+#%% Generating animation
+
+print('Generating animation of the velocity regressions')
+# Evaluate this on a new grid
+n_x = 100
+n_y = 50
+x_g = np.linspace(np.min(X_p), np.max(X_p), n_x)
+y_g = np.linspace(np.min(Y_p), np.max(Y_p), n_y)
+
+X_g, Y_g = np.meshgrid(x_g, y_g)
+# Get the solution on this new grid
+
+for idx in tqdm(range(100)):
+    # Load the weights and assign them to the Spicy object
+    weights = np.genfromtxt((Fol_Rbf + os.sep + file_names[idx]))
+    SP.w_list = [weights[:, 0], weights[:, 1]]
+    # Get the solution on the new grid
+    U_reg, V_reg = SP.get_sol(points=[X_g.ravel(), Y_g.ravel()], order=0, shape=(X_g.shape))
+
+    # Show PTV and RBF regression of the velocity data
+    fig, axes = plt.subplots(2, figsize=(6, 6), dpi=100, sharex=True, sharey=True)
+
+    axes[0].set_title('PTV Field')
+    axes[0].scatter(X_p, Y_p, c=np.sqrt(U_p**2 + V_p**2), vmin=0, vmax=18)
+    axes[0].quiver(X_p, Y_p, U_p, V_p)
+
+    axes[1].set_title('RBF Regression')
+    axes[1].contourf(X_g, Y_g, np.sqrt(U_reg**2 + V_reg**2), levels=30, vmin=0, vmax=18)
+    axes[1].quiver(X_g, Y_g, U_reg, V_reg)
+
+    for ax in axes:
+        ax.set_aspect('equal')
+        ax.set_xlabel('$x[mm]$', fontsize=13)
+        ax.set_ylabel('$y[mm]$', fontsize=13)
+        ax.set_xticks(np.arange(0, 70, 10))
+        ax.set_yticks(np.arange(-10, 11, 10))
+        ax.set_xlim([0, 50])
+        ax.set_ylim(-10, 10)
+        circle = plt.Circle((0, 0), 2.5, fill=True, color='r', edgecolor='k', alpha=0.5)
+        ax.add_patch(circle)
+
+    Name = Fol_Out_A + os.sep + 'Snapshot_U_{0:03d}.png'.format(idx)
+    fig.savefig(Name,dpi=100)
+    plt.close('all')
+
+#%% Animation of the RBF regression
+# Create the gif with the 100 saved snapshots
+# prepare the GIF
+import imageio # powerful library to build animations
+gifname = 'PTV_vs_RBF.gif'
+
+images=[]
+# 3. Pile up the images into a video
+for k in range(100):
+    print('Mounting Im '+ str(k)+' of ' + str(100))
+    Name = Fol_Out_A + os.sep + 'Snapshot_U_{0:03d}.png'.format(idx)
+    images.append(imageio.imread(Name))
+
+# 4. Create the gif from the video tensor
+imageio.mimsave(gifname, images, duration=0.1, loop=0)
